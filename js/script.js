dc83eed187c14315dd32dc1be66e26c8ee6f5d95
Mic <michail.tal@inbox.ru>
2020-11-19 21:51:45 +0200

    before calc all changes

--- js/script.js
+++ js/script.js
@@ -97,35 +97,41 @@ document.addEventListener('DOMContentLoaded', () => {
     //modal
     const 
         btnModalShow = document.querySelectorAll('[data-modal]'),
-        btnModalClose = document.querySelectorAll('[data-close]'),
+        //1 делегируем событие, чтобы учитывались и динамические кнопки и вместо
+        //btnModalClose = document.querySelectorAll('[data-close]'), используем проверку свойства
+        //а не навешиваем обработкчик на кнопку Close
+
         modal = document.querySelector('.modal');
     
     function showModal() {
         //alert('click');
-        // if (modal.classList.contains('hide')) {
-        //     modal.classList.remove('hide');
-        // }
-        // modal.classList.add('show');
-        modal.classList.toggle('show');
+        //if (modal.classList.contains('hide')) {
+            modal.classList.remove('hide');
+        //}
+         modal.classList.add('show');
+        //modal.classList.toggle('show');
         document.body.style.overflow = 'hidden';
         clearInterval(modalTimerId);
     }
     function closeModal() {
         //alert('click');
-        modal.classList.toggle('show');
-        //modal.classList.remove('show');
-        //modal.classList.add('hide');
+        //modal.classList.toggle('show');
+        modal.classList.remove('show');
+        modal.classList.add('hide');
         document.body.style.overflow = '';
     }
     btnModalShow.forEach( item => {
         item.addEventListener('click', showModal);
     });
+    /*1/ вместо этого добавим проверку в modal.addEventListener
+    evt.target.getAtribute('data-close') == ''
     btnModalClose.forEach( item => {
         item.addEventListener('click', closeModal);
     });
-    
+    */
     modal.addEventListener('click', (evt) => {
-        if (evt.target === modal) {
+        if (evt.target === modal || evt.target.getAttribute('data-close') == '') {
+            //console.log(evt.target.getAttribute('data-close'));
             closeModal();
         }
     });
@@ -137,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // modal open to 25 sec and listed site end
-    //const modalTimerId = setTimeout (showModal, 3000);
+    const modalTimerId = setTimeout (showModal, 50000);
     
     function showModalByScroll(){
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
@@ -208,6 +214,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }*/
     }
 
+    const getResources = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url} status is ${res.status}`);
+        }
+
+        return await res.json();
+    };
+    //меню с сервера - массив + его перебор
+    /*getResources('http://localhost:3000/menu')
+        .then( data => {
+            data.forEach(({src, alt, subtitle, descr, total}) => {//деструктуризация объекта
+                new MenuItem(src, alt, subtitle, descr, total, '.menu .container').render();
+            });
+        });
+        перепишем на axios*/
+    axios.get('http://localhost:3000/menu')
+        .then( data => {
+            data.data.forEach(({src, alt, subtitle, descr, total}) => {//деструктуризация объекта
+                new MenuItem(src, alt, subtitle, descr, total, '.menu .container').render();
+            });
+        });
+    /*переделали на функцию getResources работу с сервером     
     const 
         vegy = new MenuItem(
             'img/tabs/vegy.jpg',
@@ -232,10 +262,10 @@ document.addEventListener('DOMContentLoaded', () => {
             '.menu__field .container'
         );
 
-        vegy.render();
-        elite.render();
-        post.render();
-        
+    vegy.render();
+    elite.render();
+    post.render();
+    */
     /*let elemVegy = vegy.makeElem();
     let elemElite = elite.makeElem();
     let elemPost = post.makeElem();
@@ -244,4 +274,257 @@ document.addEventListener('DOMContentLoaded', () => {
     div.innerHTML = elemVegy;
     menuContainer.append(div);
     //div.innerHTML = */
+
+    //forms
+    const 
+        forms = document.querySelectorAll('form'),
+        message = {
+            loading: "img/forms-modal/spinner.svg",
+            success: 'мы с вами свяжемся в ближайшее время',
+            failure: "ошибка, повторите попытку"
+        };
+    
+    forms.forEach(form => {
+        bindPostData(form);
+    });
+    
+    const postData = async (url, data) => {
+        const res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-type': 'application/json' //закоммент так как отправляем не json 
+            },
+            //body: formData //т.к. мы еще неумеем возвращать json
+            body: data //если JSON то так
+        });
+        return await res.json();
+    };
+
+    function bindPostData(form) {
+        form.addEventListener('submit',(evt) => {
+            evt.preventDefault();
+            let formData = new FormData(form);
+            //add spinner whithout text
+            //const statusMessage = document.createElement('div');
+            //statusMessage.textContent = message.loading;
+            const statusMessage = document.createElement('img');          
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText = `
+                display: block;
+                margin: 0 auto;
+            `; //best to css.
+
+            //чтобы спиннер был в центре однотипно вместо form.append(statusMessage);
+            form.insertAdjacentElement('afterend', statusMessage);
+           /*используем fetch вместо HTTP Request
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php'); 
+            и ниже вместо request.addEventListener('load', () => { идет then(/// */
+            
+/*если используется XMLHttpRequest и form-data то заголовок устанавливать не нужно,
+он устанавливается автоматически
+            request.setRequestHeader('Content-type', 'multipart/form-data');
+    а для json нужен ужезаголовок
+*/
+
+            //request.setRequestHeader('Content-type', 'application/json');
+//тоже для json
+/*т.к. отправляем formData То это не надо: */
+            /*const obj = {};
+
+            formData.forEach( function(value, key){
+                obj[key] = value;
+            });
+            заменяем на: */
+            const json = JSON.stringify(Object.fromEntries(formData.entries()));
+            /* вынесли в функцию.
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json' //закоммент так как отправляем не json 
+                },
+                //body: formData //т.к. мы еще неумеем возвращать json
+                body: JSON.stringify(obj) //если JSON то так
+            })*/
+            postData('http://localhost:3000/requests',json)
+            //.then( data => data.text()) не нужно трансформировать, так как она идет в ф-и
+            .then(data => {
+                console.log(data); //data данные которые возвращает сервер
+                showThanksModal(message.success);
+                statusMessage.remove(); //delete spinner
+            }).catch( () => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            });
+            /*const json = JSON.stringify(obj);
+            request.send(json);*/
+
+            // for data form --> request.send(formData);
+/*
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    //вместо statusMessage.textContent = message.success; исп ф-ю
+                    showThanksModal(message.success);
+                    form.reset();
+                    statusMessage.remove(); //delete spinner
+                } else {
+                    //вместо textContent = message.failure; исп ф-юstatusMessage.
+                    showThanksModal(message.failure);
+                }
+            });*/
+        });
+    }
+
+    function showThanksModal (message) {
+        //can we hide last dualog and show new dialog
+        const prevModalDialog = document.querySelector('.modal__dialog');
+        prevModalDialog.classList.add('hide');
+        prevModalDialog.classList.remove('show');
+        showModal();
+        const thanksModal = document.createElement('div');
+        thanksModal.classList.add('modal__dialog');
+        thanksModal.innerHTML = `
+            <div class="modal__content">
+                <div data-close class="modal__close">&times;</div>
+                <div class="modal__title">${message}</div>
+            </div>    
+        `;
+        document.querySelector('.modal').append(thanksModal);
+        setTimeout( () => {
+            thanksModal.remove();
+            //prevModalDialog.classList.add('show');
+            prevModalDialog.classList.remove('hide');
+            closeModal();
+        }, 1000);
+    }
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
+    //slider
+    const
+        Slider = document.querySelector('.offer__slider'),
+        prev = Slider.querySelector('.offer__slider-prev'),
+        next = Slider.querySelector('.offer__slider-next'),
+        offerSlide = Slider.querySelectorAll('.offer__slide'),
+        current = Slider.querySelector('#current'),
+        total = Slider.querySelector('#total');
+
+//console.log(parseInt(current.textContent));
+    //let offerSlideArray =[];
+    current.textContent ='01'
+    let cur = parseInt(current.textContent);
+    offerSlide.forEach( (item, i) => {
+        if (i != cur-1) {
+            item.classList.add('hide');
+            //item.classList.remove('show');
+        }
+        //offerSlideArray.push(item);
+    });
+    //let numTotal = offerSlide.length;
+    let numTotal = offerSlide.length;
+    //console.log(offerSlideArray);
+    if (numTotal < 10 ) {
+        total.textContent = `0${numTotal}`;
+    } else {
+        total.textContent = numTotal;
+    }
+
+    Slider.style.position = 'relative';
+    const indicators = document.createElement('ol');
+    const dots = [];
+    indicators.classList.add('carousel-indicators');
+    Slider.append(indicators);
+
+    for (let i = 0; i < numTotal; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.classList.add('dot');
+        if (i==0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    function prevSlide() {
+        let cur = parseInt(current.textContent);
+
+        cur = (cur-1 < 1) ? numTotal : cur-1;
+
+        if (cur < 10) {
+            current.textContent = `0${cur}`;
+        } else {
+            current.textContent = cur;
+        }
+        showSlide (cur);
+        viewActiveDot(cur);
+    }
+
+    function nextSlide() {
+        let cur = parseInt(current.textContent);
+
+        cur = (cur+1 > numTotal) ? 1 : cur+1;
+
+        if (cur < 10) {
+            current.textContent = `0${cur}`;
+        } else {
+            current.textContent = cur;
+        }
+        showSlide (cur);
+        viewActiveDot(cur);
+    }
+
+    function showSlide (numSlide) {
+        offerSlide.forEach( (item, i) => {
+            if (i == numSlide-1) {
+                item.classList.remove('hide');
+                item.classList.add('show');
+            } else {
+                item.classList.add('hide');
+                item.classList.remove('show');
+            }
+        });
+    }
+
+    function viewActiveDot(numDot){
+        dots.forEach(dot => {
+            dot.style.opacity = '0.5';
+        });
+        dots[numDot-1].style.opacity = 1;
+    }
+    
+    prev.addEventListener('click',prevSlide);
+    next.addEventListener('click',nextSlide);
+
+    dots.forEach (dot => {
+        dot.addEventListener('click', (evt) => {
+            const slideTo = evt.target.getAttribute('data-slide-to');
+//console.log(slideTo);
+            cur = slideTo;
+            offerSlide.forEach( (item, i) => {
+                if (i == cur-1) {
+                    item.classList.remove('hide');
+                    item.classList.add('show');
+                } else {
+                    item.classList.add('hide');
+                    item.classList.remove('show');
+                }
+            });
+            dots.forEach(dot => {
+                dot.style.opacity = '0.5';
+            });
+            dots[cur-1].style.opacity = 1;
+            if (cur < 10) {
+                current.textContent = `0${cur}`;
+            } else {
+                current.textContent = cur;
+            }
+        });
+    });
+
+    //calc
+    
 });
